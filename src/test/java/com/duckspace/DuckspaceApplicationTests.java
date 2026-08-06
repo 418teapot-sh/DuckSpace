6008aae85d7212aad73fbcac60a0f449b59169d1
Seunghwan Song <bourne11594@gmail.com>
2026-08-06 15:06:23 +0900

    [Chore]: 프로젝트 초기 설정

---
+++ src/test/java/com/duckspace/DuckspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duckspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuckspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
